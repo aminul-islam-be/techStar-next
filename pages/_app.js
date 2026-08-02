fc2457e7febfe7b14fb45f279202435292017a63
aminul-islam-be <112537464+aminul-islam-be@users.noreply.github.com>
2026-08-02 18:08:09 +0600

    Include viewport meta tag in _app.js
    Add viewport meta tag for responsive design.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );

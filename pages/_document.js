e4ecd923f8d2132dd748307f0a1869b66ad9f4aa
aminul-islam-be <112537464+aminul-islam-be@users.noreply.github.com>
2026-08-02 18:08:56 +0600

    Refactor Document component in _document.js
    Removed viewport meta tag and fixed formatting.

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="International Electrical & Electronics E-commerce Platform" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -14,5 +13,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
-    }
+  );
+}

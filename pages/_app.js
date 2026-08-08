83ebd8003f57889e06a20817ef33f1b4c125e9c2
aminul-islam-be <112537464+aminul-islam-be@users.noreply.github.com>
2026-08-08 09:21:00 +0600

    Add AIChatbot component to MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
-import Head from 'next/head';
-import { SessionProvider } from 'next-auth/react';
-import '../styles/globals.css';
+import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
+import AIChatbot from "../components/AIChatbot";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={session}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" />
-      </Head>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <SessionProvider session={session}> <Head> <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=yes" /> </Head> <Component {...pageProps} /> <AIChatbot /> </SessionProvider>
   );
 }
 
